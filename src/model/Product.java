9faaa7cbccbc068b0f56eef994670081e3e756a1
Thimmothy <10700470@ucn.dk>
2026-03-11 10:41:31 +0100

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,54 @@
 package model;
 
 public abstract class Product {
+	
+	private int productNumber;
+	private String name;
+	private int minStock;
+	private int reserveQty;
+	
+	public Product(int productNumber, String name, int minStock, int reserveQty){
+		this.productNumber = productNumber;
+		this.name = name;
+		this.minStock = minStock;
+		this.reserveQty = reserveQty;
+	}
+	
+	public Product() {
+		
+	}
 
+	public int getProductNumber() {
+		return productNumber;
+	}
+
+	public void setProductNumber(int productNumber) {
+		this.productNumber = productNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+
+	public int getReserveQty() {
+		return reserveQty;
+	}
+
+	public void setReserveQty(int reserveQty) {
+		this.reserveQty = reserveQty;
+	}
+	
+	
 }
