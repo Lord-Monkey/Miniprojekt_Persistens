0635582c280b3ffadebf0129ad2c137ccf7c269a
Norden-Toft <10700469@ucn.dk>
2026-03-11 12:26:36 +0100

    saleOrder update

--- src/db/SaleOrderDB.java
+++ src/db/SaleOrderDB.java
@@ -2,6 +2,7 @@ package db;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.SaleOrder;
@@ -12,20 +13,37 @@ import model.SaleOrder;
  * @author Christian Nordentoft
  * @version 11-3-2026
  */
-public class SaleOrderDB {
+public class SaleOrderDB implements SaleOrderDBIF {
 	
-	private String FIND_ALL_Q;
+	private static final String FIND_ALL_Q = "select orderNo, date, deliveryStatus, deliveryDate,"
+			+ " discountGiven, c, orderLines from SaleOrder ";
 	private PreparedStatement findAllPS;
-	private String insert_Q;
+	
+	private static final String INSERT_Q = "insert into SaleOrder(orderNo, date, deliveryStatus,"
+			+ " deliveryDate, discountGiven, c, orderLines)";
 	private PreparedStatement INSERT_PS;
 	
-	public SaleOrderDB(){
-		 
+	public SaleOrderDB() throws DataAccessException{
+		try {
+			findAllPS = DBConnection.getInstance().getConnection().prepareStatement(FIND_ALL_Q);
+			INSERT_PS = DBConnection.getInstance().getConnection().prepareStatement(INSERT_Q);
+		} catch (SQLException e) {
+			throw new DataAccessException("Connection issue", e);
+		}
 	}
 	
-	public List<SaleOrderDB> findAll() {
-		
+	public List<SaleOrder> findAll() throws DataAccessException{
+		ResultSet rs;
+		try {
+			rs = findAllPS.executeQuery();
+			List<SaleOrder> res = buildObjects(rs);
+			return res;
+		} catch(SQLException e) {
+			throw new DataAccessException("Couldn't find all saleorders", e);
+		}
 	}
+		
+	
 	
 	private List<SaleOrder> buildObjects(ResultSet rs){
 		
@@ -35,7 +53,7 @@ public class SaleOrderDB {
 		
 	}
 	
-	public boolean insert(SalesOrder so) {
+	public boolean insert(SaleOrder so) {
 		
 	}
 }
