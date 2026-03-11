6eedbb333f037fcab12e47aaa10726c86fd16b06
Thimmothy <10700470@ucn.dk>
2026-03-11 11:40:21 +0100

    Update OrderLineItemDBIF.java

--- src/db/OrderLineItemDBIF.java
+++ src/db/OrderLineItemDBIF.java
@@ -1,5 +1,13 @@
 package db;
 
+import java.util.List;
+
+import model.OrderLineItem;
+
 public interface OrderLineItemDBIF {
+	
+	Boolean insert(OrderLineItem ol) throws DataAccessException;
+	
+	List<OrderLineItem> findAll() throws DataAccessException;
 
 }
