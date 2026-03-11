05987dd9077cb6d9ff3237c3079b258f24503f3f
Mightypaw <10700499@ucn.dk>
2026-03-11 12:29:33 +0100

    Update ProductDB.java

--- src/db/ProductDB.java
+++ src/db/ProductDB.java
@@ -1,5 +1,37 @@
 package db;
 
-public class ProductDB {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Product;
+
+public class ProductDB implements ProductDBIF {
+
+	private static final String FIND_ALL_Q =
+			"select type, productNumber, name, minStock, reservedStock from Product";
+	private static final String FIND_BY_PRODUCTNUMBER =
+			FIND_ALL_Q + "where id = ?";
+	private PreparedStatement findAllPs, findByPN;
+	
+	public ProductDB() throws DataAccessException {
+		try{
+			findAllPs = DBConnection.getInstance().getConnection().prepareStatement(FIND_ALL_Q);
+		} catch (SQLException e) {
+			throw new DataAccessException("Could not prepare statement", e);
+		}
+	}
+
+	
+	public Product findProduct(int productNumber) {
+		
+		return null;
+	}
+
+
+	public List<Product> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
