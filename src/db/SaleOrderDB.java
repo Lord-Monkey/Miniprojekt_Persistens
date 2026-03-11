d4dbe5826b14d8bfe60024c9be3a50ace71a8eb9
Norden-Toft <10700469@ucn.dk>
2026-03-11 13:01:33 +0100

    Update SaleOrderDB.java

--- src/db/SaleOrderDB.java
+++ src/db/SaleOrderDB.java
@@ -42,11 +42,15 @@ public class SaleOrderDB implements SaleOrderDBIF {
 			throw new DataAccessException("Couldn't find all saleorders", e);
 		}
 	}
-		
-	
 	
 	private List<SaleOrder> buildObjects(ResultSet rs) throws SQLException {
-	    
+		List<SaleOrder> foundSaleOrder = new java.util.ArrayList<SaleOrder>();
+		SaleOrder tempSaleOrder = null;
+		while(rs.next()) {
+			tempSaleOrder = buildObject(rs);
+			foundSaleOrder.add(tempSaleOrder);
+		}
+		return foundSaleOrder;
 	}
 	
 	private SaleOrder buildObject(ResultSet rs) {
