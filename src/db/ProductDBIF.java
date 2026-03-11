a82e87a6d6188bb9fcf2f3efbc95c50734b3c286
Mightypaw <10700499@ucn.dk>
2026-03-11 10:51:39 +0100

    Update ProductDBIF.java

--- src/db/ProductDBIF.java
+++ src/db/ProductDBIF.java
@@ -1,5 +1,11 @@
 package db;
 
+import java.util.List;
+
+import model.Product;
+
 public interface ProductDBIF {
 
+	public Product findProduct(int productNumber);
+	public List<Product> findAll();
 }
