0cd1921815bd4fb1d48719bd45cea16f813efe36
Lord-Monkey <10632163@ucn.dk>
2026-03-12 09:14:39 +0100

    Update OrderLineItemDB.java

--- src/db/OrderLineItemDB.java
+++ src/db/OrderLineItemDB.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
 
 import model.OrderLineItem;
 import model.Product;
@@ -13,11 +14,10 @@ public class OrderLineItemDB implements OrderLineItemDBIF {
 	private static final String FIND_ALL_Q = "select id, orderId, productNumber, quantity"
 			+ "From OrderLineItem";
 	private PreparedStatement findAllPS;
-	
-	private static final String INSERT_Q = "insert into OrderLineItem (orderId,"
-			+ " productNumber, quantity) values (?, ?, ?)";
+	private static final String INSERT_Q = "insert into OrderLineItem (orderId, productId, quantity) values (?, ?, ?)";
 	private PreparedStatement INSERT_PS;
-	private ProductDB productDB;
+	private ProductDBIF productDB;
+	private ArrayList<OrderLineItem> orderLineItems;
 	
 	public OrderLineItemDB() throws DataAccessException {
 		try {
@@ -30,9 +30,19 @@ public class OrderLineItemDB implements OrderLineItemDBIF {
 	}
 
 	@Override
-	public Boolean insert(OrderLineItem ol) throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean insert(OrderLineItem oli, int orderNo) throws DataAccessException {
+		try {
+			
+			INSERT_PS.setInt(1, oli.getProduct().getProductNumber());
+			INSERT_PS.setInt(2, oli.getQuantity());
+			int res = INSERT_PS.executeUpdate();
+			if(res > 0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException("Couldn't insert order line item", e);
+		}
+		return 	null;
 	}
 
 	@Override
@@ -47,8 +57,8 @@ public class OrderLineItemDB implements OrderLineItemDBIF {
 		}
 	}
 	
-	public List<OrderLineItem> buildObjects(ResultSet rs) throws Exception {
-		List<OrderLineItem> foundOrderLineItems = new java.util.ArrayList<OrderLineItem>();
+	private List<OrderLineItem> buildObjects(ResultSet rs) throws Exception {
+		List<OrderLineItem> foundOrderLineItems = new ArrayList<OrderLineItem>();
 		OrderLineItem tempOrderLineItem = null;
 		while(rs.next()) {
 			tempOrderLineItem = buildObject(rs);
@@ -57,17 +67,13 @@ public class OrderLineItemDB implements OrderLineItemDBIF {
 		return foundOrderLineItems;
 	}
 	
-	public OrderLineItem buildObject(ResultSet rs) throws SQLException {
+	private OrderLineItem buildObject(ResultSet rs) throws SQLException, DataAccessException {
 		Product p;
 		int quantity;
-		
 		int productNumber = rs.getInt("productNumber");
 		quantity = rs.getInt("quantity");
-		
 		p = productDB.findProduct(productNumber);
-		
-		OrderLineItem ol = new OrderLineItem(p, quantity);
-		return ol;
+		OrderLineItem oli = new OrderLineItem(p, quantity);
+		return oli;
 	}
-
 }
