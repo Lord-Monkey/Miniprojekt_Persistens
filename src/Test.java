b068473a0380e6b1012fab61201f10b48257aff2
Mightypaw <10700499@ucn.dk>
2026-03-12 13:28:50 +0100

    Create Test.java

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+import controller.CustomerCtr;
+import controller.ProductCtr;
+import controller.SalesOrderCtr;
+import db.DataAccessException;
+
+public class Test {
+
+	public static void main(String[] args) throws DataAccessException {
+		CustomerCtr cctr = new CustomerCtr();
+		SalesOrderCtr soctr = new SalesOrderCtr();
+		ProductCtr pctr = new ProductCtr();
+		
+		if(cctr != null) {
+			System.out.println("cctr works");
+		}
+		
+		if(soctr != null) {
+			System.out.println("soctr works");
+			
+		if(pctr != null) {
+			System.out.println("pctr works");
+		}
+		}
+	}
+
+}
