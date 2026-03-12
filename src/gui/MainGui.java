d116a301505b2214c46d0270fe34c210afa57330
Mightypaw <10700499@ucn.dk>
2026-03-12 10:22:31 +0100

    ui fun

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -1,10 +1,16 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Insets;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainGui extends JFrame {
 
@@ -36,6 +42,38 @@ public class MainGui extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_North = new JPanel();
+		contentPane.add(panel_North, BorderLayout.NORTH);
+		
+		JPanel panel_Center = new JPanel();
+		contentPane.add(panel_Center, BorderLayout.CENTER);
+		panel_Center.setLayout(new MigLayout("", "[85px][85px]", "[21px][][][][][][]"));
+		
+		JButton btnOrders = new JButton("Ordrer");
+		btnOrders.setMargin(new Insets(2, 31, 2, 31));
+		btnOrders.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		panel_Center.add(btnOrders, "cell 0 1,alignx left,aligny top");
+		
+		JButton btn_Customer = new JButton("Kunder");
+		btn_Customer.setMargin(new Insets(2, 30, 2, 30));
+		panel_Center.add(btn_Customer, "cell 0 2");
+		
+		JButton btn_Products = new JButton("Produkter");
+		btn_Products.setMargin(new Insets(2, 24, 2, 24));
+		panel_Center.add(btn_Products, "cell 0 3");
+		
+		JButton btn_Notifications = new JButton("Notifikationer");
+		btn_Notifications.setMargin(new Insets(2, 16, 2, 16));
+		panel_Center.add(btn_Notifications, "cell 0 4,alignx left,aligny top");
+		
+		JPanel panel_South = new JPanel();
+		contentPane.add(panel_South, BorderLayout.SOUTH);
 
 	}
 
