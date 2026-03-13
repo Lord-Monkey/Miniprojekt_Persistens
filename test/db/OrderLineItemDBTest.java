c2e61c81dd420ee659852ad028182da8ca289754
Lord-Monkey <10632163@ucn.dk>
2026-03-13 09:11:27 +0100

    Update OrderLineItemDBTest.java

--- test/db/OrderLineItemDBTest.java
+++ test/db/OrderLineItemDBTest.java
@@ -1,5 +1,24 @@
 package db;
 
+import model.OrderLineItem;
+import model.Product;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class OrderLineItemDBTest {
+	
+    
+	@Test
+	void testInsertReturnsTrue() throws DataAccessExecption {
+		//Arrange
+		OrderLineItemDB orderLineItemDB = new OrderLineItemDB();
+		
+		//Act
+		
+		
+		//Assert
+	}
 
 }
